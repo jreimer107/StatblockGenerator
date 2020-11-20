1d9ee54f9af45693d589ce178966f47bbb87745f
John Reimer <jreimer107@gmail.com>
2020-11-19 22:36:54 -0600

    Added XP functionality

--- statblock_generate.py
+++ statblock_generate.py
@@ -3,22 +3,65 @@ from sys import argv
 import pyperclip
 
 ABILITY_MODIFIERS = ["-5", "-5", "-4", "-4", "-3", "-3", "-2", "-2", "-1", "-1", "+0", "+0", "+1", "+1", "+2", "+2", "+3", "+3", "+4", "+4", "+5", "+5", "+6", "+6", "+7", "+7", "+8", "+8", "+9", "+9", "+10"]
+XP_BY_CR = {
+    "0": "10",
+    "1/8": "25",
+    "1/4": "50",
+    "1/2": "100",
+    "1": "200",
+    "2": "450",
+    "3": "700",
+    "4": "1,100",
+    "5": "1,800",
+    "6": "2,300",
+    "7": "2,900",
+    "8": "3,900",
+    "9": "5,000",
+    "10": "5,900",
+    "11": "7,200",
+    "12": "8,400",
+    "13": "10,000",
+    "14": "11,500",
+    "15": "13,000",
+    "16": "15,000",
+    "17": "18,000",
+    "18": "20,000",
+    "19": "22,000",
+    "20": "25,000",
+    "21": "33,000",
+    "22": "41,000",
+    "23": "50,000",
+    "24": "62,000",
+    "25": "75,000",
+    "26": "90,000",
+    "27": "105,000",
+    "28": "120,000",
+    "29": "135,000",
+    "30": "155,000"
+}
 
 def get_mod(stat: str) -> str:
     return (ABILITY_MODIFIERS[int(stat)])
 
-target_monster = None
-if len(argv) >= 2:
-    target_monster = argv[1]
-else:
-    target_monster = input("Enter a monster name: ")
 
-reader = csv.reader(open("Monster Spreadsheet (D&D5e).csv", "r"))
-for row in reader:
-    if row[0] == target_monster:
-        name, size, monster_type, align, ac, hp, speeds, strength, dex, con, intel, wis, cha, saving_throws, skills, wri, senses, languages, cr, additional, font, additional_info, author = row;
-        
-        formatted_string = "___\n\
+def main():
+    target_monster = None
+    if len(argv) >= 2:
+        target_monster = argv[1]
+    else:
+        target_monster = input("Enter a monster name: ")
+
+    reader = None
+    try:
+        reader = csv.reader(open("monsterData.csv", "r"))
+    except FileNotFoundError:
+        print("ERROR: CSV not found.")
+        exit(1)
+
+    for row in reader:
+        if row[0] == target_monster:
+            name, size, monster_type, align, ac, hp, speeds, strength, dex, con, intel, wis, cha, saving_throws, skills, wri, senses, languages, cr, additional, font, additional_info, author = row
+            formatted_string = "___\n\
 > ## %s\n\
 >*%s %s, %s*\n\
 > ___\n\
@@ -38,7 +81,7 @@ for row in reader:
 > - **Condition Immunities** condition_Immunities\n\
 > - **Senses** %s\n\
 > - **Languages** %s\n\
-> - **Challenge** %s and XP\n\
+> - **Challenge** %s (%s XP)\n\
 > ___\n\
 >\n\
 > ### Actions\n\
@@ -46,12 +89,15 @@ for row in reader:
 >\n\
 > ***Ability Description.*** *Attack Style:* Attack Bonus to hit, Reach/Range, one target. *Hit:* Damage Damage Type damage\n\
 >\n\
-> ***General Ability Description.*** General Attack Description" % (name, size, monster_type, align, ac, hp, speeds, strength, get_mod(strength), dex, get_mod(dex), con, get_mod(con), intel, get_mod(intel), wis, get_mod(wis), cha, get_mod(cha), saving_throws, skills, senses, languages, cr)
+> ***General Ability Description.*** General Attack Description" % (name, size, monster_type, align, ac, hp, speeds, strength, get_mod(strength), dex, get_mod(dex), con, get_mod(con), intel, get_mod(intel), wis, get_mod(wis), cha, get_mod(cha), saving_throws, skills, senses, languages, cr, XP_BY_CR[cr])
 
-        print(formatted_string)
-        pyperclip.copy(formatted_string)
+            print(formatted_string)
+            pyperclip.copy(formatted_string)
 
-        exit()
+            exit()
 
 print("Monster not found.")
+
+if __name__ == "__main__":
+    main()
 
